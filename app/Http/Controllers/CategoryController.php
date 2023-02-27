9bf49b60f3e2bea4b3fc9228b837b80f4a36fb53
nurmukhamed <nurkamalnurmukhamed@gmail.com>
2023-02-27 16:27:40 +0600

    feat: recursively get category tree

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,6 +14,23 @@ class CategoryController extends Controller
         return response()->json(CategoryResource::collection($categories));
     }
 
+    public function getCategories($parent_id = null)
+    {
+        $categories = Category::where('parent_id', $parent_id)->get();
+        $result = [];
+
+        foreach ($categories as $category) {
+            $children = $this->getCategories($category->id); // Рекурсивный вызов для получения дочерних категорий
+            $result[] = [
+                'id' => $category->id,
+                'name' => $category->name,
+                'children' => $children
+            ];
+        }
+
+        return $result;
+    }
+
     public function getChilds(Request $request) {
         $categories = Category::where('parent_id', $request->id)->get();
         return response()->json(CategoryResource::collection($categories));
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories', [CategoryController::class, 'getCategories']);
     Route::get('/categories/{id}', [CategoryController::class, 'getChilds']);
 
     Route::get('products/{param}', [ProductController::class, 'show']);
